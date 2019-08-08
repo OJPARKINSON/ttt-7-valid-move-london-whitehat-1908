a6c36e3050cc2c47ae2613a97f2b39d6e5281424
Oliver Parkinson <olliepar01@gmail.com>
2019-08-08 08:33:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move(board, index)
-  if between(0,8) && board[index] = " " then 
+  if between?(0,8) && board[index] = " " then 
     return true
   else 
     return false
